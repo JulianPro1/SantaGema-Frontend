8039864465b085d70b2c42ae40864a74be650630
Alehernandezt <j.alehernandezt@gmail.com>
2024-08-14 16:25:11 -0400

    added chekbox in roles y permisos

--- src/app/Views/roles-create/roles-create.component.ts
+++ src/app/Views/roles-create/roles-create.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AsideBarComponent } from "../../Shared/aside-bar/aside-bar.component";
+import { FormRolesCreateComponent } from "../../Shared/forms/form-roles-create/form-roles-create.component";
 
 @Component({
   selector: 'app-roles-create',
   standalone: true,
-  imports: [],
+  imports: [AsideBarComponent, FormRolesCreateComponent],
   templateUrl: './roles-create.component.html',
   styleUrl: './roles-create.component.css'
 })
