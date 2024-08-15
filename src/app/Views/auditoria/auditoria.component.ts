e8a826a3b52bb410e848be349d15205ba3980757
JulianPro1 <jjosealejos07@gmail.com>
2024-08-15 10:48:12 -0400

    advance audit management

--- src/app/Views/auditoria/auditoria.component.ts
+++ src/app/Views/auditoria/auditoria.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { H1Component } from "../../Shared/h1/h1.component";
+import { AsideBarComponent } from "../../Shared/aside-bar/aside-bar.component";
+import { H3Component } from "../../Shared/h3/h3.component";
 
 @Component({
   selector: 'app-auditoria',
   standalone: true,
-  imports: [],
+  imports: [H1Component, AsideBarComponent, H3Component],
   templateUrl: './auditoria.component.html',
   styleUrl: './auditoria.component.css'
 })
