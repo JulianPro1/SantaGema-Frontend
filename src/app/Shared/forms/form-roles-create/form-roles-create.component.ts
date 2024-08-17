0c7e1a2f31349a3211721b337b687a6644d1021c
Alehernandezt <j.alehernandezt@gmail.com>
2024-08-16 20:19:15 -0400

    modal not working yet but in progress

--- src/app/Shared/forms/form-roles-create/form-roles-create.component.ts
+++ src/app/Shared/forms/form-roles-create/form-roles-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { H1Component } from "../../h1/h1.component";
 import { H2Component } from "../../h2/h2.component";
 import { InputTextComponent } from "../input-text/input-text.component";
@@ -6,9 +6,7 @@ import { TextareaComponent } from "../textarea/textarea.component";
 import { InputDesplegableComponent } from "../input-desplegable/input-desplegable.component";
 import { ButtonComponent } from "../../button/button.component";
 import { CheckboxComponent } from "../checkbox/checkbox.component";
-import {
-  MatDialog,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ModalRolesComponent } from '../modal-roles/modal-roles.component';
 
 @Component({
@@ -22,24 +20,24 @@ import { ModalRolesComponent } from '../modal-roles/modal-roles.component';
     InputDesplegableComponent, 
     ButtonComponent, 
     CheckboxComponent
-  
   ],
   templateUrl: './form-roles-create.component.html',
-  styleUrl: './form-roles-create.component.css'
+  styleUrls: ['./form-roles-create.component.css']
 })
-export class FormRolesCreateComponent {
+export class FormRolesCreateComponent implements OnInit {
   readonly dialog = inject(MatDialog);
+  selectedOptions = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
   openDialog(): void {
     this.dialog.open(ModalRolesComponent, {
       data: {},
+    }).afterClosed().subscribe(result => {
+      this.selectedOptions = result;
     });
-
-    // dialogRef.afterClosed().subscribe(result => {
-    //   if (result !== undefined) {
-    //     this.animal.set(result);
-    //   }
-    };
   }
-
+}
--- src/app/Shared/forms/modal-roles/modal-roles.component.ts
+++ src/app/Shared/forms/modal-roles/modal-roles.component.ts
@@ -1,19 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject, model } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
-import {MatDialogActions, MatDialogContent, MatDialogClose} from '@angular/material/dialog';
-
-
+import {MatDialogActions, MatDialogContent, MatDialogClose, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { CheckboxComponent } from "../checkbox/checkbox.component";
+import { H2Component } from "../../h2/h2.component";
+import RolesCreateComponent from '../../../Views/roles-create/roles-create.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal-roles',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatDialogActions, MatDialogContent, MatDialogClose],
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule, 
+    MatButtonModule, 
+    MatDialogActions, 
+    MatDialogContent, 
+    MatDialogClose, 
+    CheckboxComponent, 
+    H2Component, 
+    CommonModule 
+  ],
   templateUrl: './modal-roles.component.html',
   styleUrl: './modal-roles.component.css'
 })
 export class ModalRolesComponent {
 
+  constructor(private dialogRef: MatDialogRef<ModalRolesComponent>, @Inject(MAT_DIALOG_DATA) public data: RolesCreateComponent) { }
+
+  selectedOptions: any = [];
+
+  onClose() {
+    this.dialogRef.close(this.selectedOptions);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close(this.selectedOptions);
+  }
+
+  onSelectedOptionsChange(options: any) {
+    this.selectedOptions = options;
+  }
 }
